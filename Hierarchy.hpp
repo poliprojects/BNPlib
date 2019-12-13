bcb7b4b790fe11ae55f0150137efc44d5fe398c7
Bruno Guindani <bruno@guindani.org>
2019-12-13 11:14:28 +0100

    Some ideas, tools, and sketches

--- Hierarchy.hpp
+++ Hierarchy.hpp
@@ -1,13 +1,47 @@
-class Distribution{
-	arma::vec parameters;
-	double eval();
+class MixingMode?{
+	...
+}
+
+class Hierarchy{
+	...
+}
+
+template<MixingMode? M, Hierarchy H>
+class Algorithm{
+	private:
+		unsigned int maxiter;
+		void step(unsigned int iter) {
+			std::cout << "Step " << iter << std::endl;
+			sample_clusters();
+			sample_parameters();
+			if(H->G0.has_prior())
+				update_hyperparams();
+			if(M.alpha_has_prior())
+				update_alpha();
+			if(iter > burnin)
+				save();
+		}
+		void sample_clusters(){...}
+		// etc...
+
+	public:
+		// Constructors...
+
+		void run(){
+			initialize();
+			unsigned int iter = 1;
+			while(!converged){
+				step(iter);
+				iter++;
+				converged = ...;
+			}
+		}
 }
 
 
-class DistributionWithHypers: public Distribution{
-	std::vector<shared_ptr<Distribution>> hyperparameters;
-	arma::vec update();
-}
+
+
+
 
 
 class params {
