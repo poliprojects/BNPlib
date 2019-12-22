095009aab7ed0be1fb5d5b31f5cd4f7d76346c9b
Elena Zazzetti <elena.zazzetti@mail.polimi.it>
2019-12-22 17:52:02 +0100

    sample_allocations

--- DistributionWrapper.hpp
+++ DistributionWrapper.hpp
@@ -1,5 +1,6 @@
 namespace bnplib{
 	using data_t = double;
+	using param_t = std::vector<double>;
 	using Gamma = std::gamma_distribution<data_t>;
 	using Normal = std::normal_distribution<data_t>;
 	// etc...
@@ -106,7 +107,8 @@ class NNIGHierarchy {
 protected:
 	std::tuple<real, real> state;
 	//std::tuple<> params; // mu_0, k, alpha, beta
-	qualcosa che mi rappresenta i parametri ma possono cambiare
+
+	//qualcosa che mi rappresenta i parametri ma possono cambiare
 	Hypers* hypers;
 public:
 	double log_like(real datum) {
@@ -116,7 +118,7 @@ public:
 	void sample(std::vector<real> data) {
 		// prendo mu_0, k0, alpha0, beta0 da hypers
 		mu_0 = hypers. // current value
-		// calcolo mu_post, k_post, alpha_post, beta_post 
+		// calcolo mu_post, k_post, alpha_post, beta_post
 
 		real sigmaNew = stan::math::inv_gamma_rng(alpha_post, beta_post, rng);
 		real muNew = stan::math::normal_rng(mu_post, kpost * sigmaNew, rng);
@@ -134,8 +136,8 @@ class Neal8 {
 	std::vector<Hierarchy> hierarchies;
 	std::vector<real> data;
 	std::array<n,int> allocations; // the c vector
-	std::vector<data_t> unique_values;
-	std::array<m,data_t> aux_unique_values;
+	std::vector<param_t> unique_values;
+	std::array<m,param_t> aux_unique_values;
 
 	Hypers hypers;
 
@@ -185,23 +187,95 @@ class Neal8 {
 	}
 
 	void sample_allocations(){
-		for(int i=0; i<n; i++){ // for each data unit
-			//data[i]
-            std::vector<unsigned int> temp;
-            // ideas: our own for loop for k and bool (ci is a singleton)
-            // ideas: function from std count distinct values in vector
-            // ideas: using a (multi)map?
-            for(int j=0; j<n; j++){
-                if(j == i)
-                    continue;
 
-            }
+		// initialize cardinalities
+		std::vector<unsigned int> card(unique_values.size(), 0); // cardinalities of unique values
 
-			int k = unique_sans_i(i);
-			int h = k+m;
+		for(int j=0; j<n; j++){
+			card[allocations[j]]+=1;
 		}
-	}
+
+
+		int k, start, n_unique;
+
+		for(int i=0; i<n; i++){ // for each data unit
+
+			//data[i]
+			// ideas: our own for loop for k and bool (ci is a singleton)
+			// ideas: function from std count distinct values in vector
+			// ideas: using a (multi)map?
+			start=0;
+			n_unique=unique_values.size();
+			if(card[allocations[i]]==1){ // datum i is a singleton
+			k=n_unique-1;
+			aux_unique_values[0]=unique_values[allocations[i]];
+			singleton=1;}
+			else k=n_unique;
+			card[allocations[i]]-=1;
+
+			for(int j=singleton;j<m; j++ ){
+			aux_unique_values[j][0] = stats::rnorm(hypers.get_m0(), hypers.get_k0());
+			aux_unique_values[j][1] = stats::rinvgamma(hypers.get_alpha0(),hypers.get_beta0());
+			}
+			std::vector<double> probas(k+m,0);
+			for(int k=0; k<n_unique ; k++){ // if datum i is a singleton card[k] when k=allocations[i] is equal to 0 ->probas[k]=0
+				probas[k]=card[k]*stats::pnorm(data[i],unique_values[k][0],unique_values[k][1])/(n-1+ mixture.get_totalmass());
+			}
+			for(int k=0; k<m ; k++){
+				probas[n_unique+k]=(mixture.get_totalmass()/m)*stats::pnorm(data[i],unique_values[k][0],unique_values[k][1])/(n-1+ mixture.get_totalmass());
+			}
+
+			int c_new= std::discrete_distribution(probas); // in stats??
+			if(singleton){
+
+				if (c_new>=n_unique){ // SINGLETON - AUX
+					unique_values[allocations[i]]=aux_unique_values[c_new-n_unique];
+					card[allocations[i]]+=1;
+				}
+				else // SINGLETON - OLD VALUE
+		    {unique_values.erase(unique_values.begin()+allocations[i]-1);
+				card.erase(card.begin()+allocations[i]-1);
+				card[c_new]+=1;
+
+				int tmp=allocations[i];
+				allocations[i]=c_new;
+				for(auto &c:allocations){ // relabeling
+					if (c>tmp)
+					c-=1;
+				}
+			}
+
+			}
+			else{
+
+				if (c_new>=n_unique){ // NOT SINGLETON - AUX
+					unique_values.push_back(aux_unique_values[c_new-n_unique]);
+					card.push_back(1);
+					allocations[i]=n_unique+1;
+			}
+			else{ // NOT SINGLETON - OLD VALUES 
+				allocations[i]=c_new;
+				card[c_new]+=1;
+			}
+
+		}
+			}
+		}
+
+	} // end sample allocation
+
+
+
+
+
 }
+
+}
+
+
+
+
+
 
 int main() {
 	Neal8<NNIGHierarchy<HypersFixed>> sampler;
@@ -221,4 +295,3 @@ class Hypers{
     real current_value;
     void sample(){...} // prende a caso
 }
-
