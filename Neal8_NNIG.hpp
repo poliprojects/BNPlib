6bdb13dcd07c067a79ea9717ee165fd91717d3eb
Bruno Guindani <bruno@guindani.org>
2019-12-22 23:37:22 +0100

    Cleanup of Neal8_NNIG

--- Neal8_NNIG.hpp
+++ Neal8_NNIG.hpp
@@ -11,41 +11,46 @@ protected:
     std::tuple<real, real> state;
     //std::tuple<> params; // mu_0, k, alpha, beta
 
+
     //qualcosa che mi rappresenta i parametri ma possono cambiare
     Hypers* hypers;
 public:
+    // getters, such as:
+    std::tuple getState();
+
     double log_like(real datum) {
         return stan::math::normal_lpdf(datum, state-1, state-2);
     }
 
     void sample(std::vector<real> data) {
-        // prendo mu_0, k0, alpha0, beta0 da hypers
-        mu_0 = hypers. // current value
-        // calcolo mu_post, k_post, alpha_post, beta_post
+        // prendo mu_0, k0, alpha0, beta0 da hypers:
+        mu_0 = hypers.current_value // ?
+
+        ... // calcolo mu_post, k_post, alpha_post, beta_post
 
         real sigmaNew = stan::math::inv_gamma_rng(alpha_post, beta_post, rng);
         real muNew = stan::math::normal_rng(mu_post, kpost * sigmaNew, rng);
         state(0) = muNew;
         state(1) = sigmaNew;
-    }
-
-    std::tuple getState();
+        }
 }
 
 
 
 template<Hierarchy, Hypers>
-class Neal8 {
-    int m=3;
+class Neal8{
+private:
+    int m=3; // TODO
     std::vector<Hierarchy> hierarchies;
     std::vector<real> data;
     std::array<n,int> allocations; // the c vector
     std::vector<param_t> unique_values;
     std::array<m,param_t> aux_unique_values;
+    
+    Hypers hypers; // see Hypers class in other files
 
-    Hypers hypers;
-
-    void sample_c {
+    // Idea by Mario
+    void sample_c() {
         for (i=0; i<numData; i++) {
             old = c[i];
             probas = Eigen::VectorXd(numClusters);
@@ -63,9 +68,9 @@ class Neal8 {
         }
     }
 
-    void sample_clustervals(){
-        for (c = 0)
-    }
+
+
+
 
     void run(){
         initalize();
@@ -84,107 +89,118 @@ class Neal8 {
         sample_unique_values();
     }
 
-    int unique_sans_i(int i){ // counts unique values by scrolling allocations
-        // without the i-th unit
-        // ...
-        return k;
-    }
 
     void sample_allocations(){
+        // Other ideas:
+        // * ideas: our own for loop for k and bool (ci is a singleton)
+        // * ideas: function from std count distinct values in vector
+        // * ideas: using a (multi)map?
 
-        // initialize cardinalities
-        std::vector<unsigned int> card(unique_values.size(), 0); // cardinalities of unique values
+        // Initialize some relevant variables
+        unsigned int k, n_unique, singleton;
 
-        for(int j=0; j<n; j++){
-            card[allocations[j]]+=1;
-        }
+        // Initialize cardinalities of unique values
+        std::vector<unsigned int> card(unique_values.size(), 0);
+        for(int j=0; j<n; j++)
+            card[ allocations[j] ] += 1;
 
+        for(int i=0; i<n; i++){ // for each data unit data[i]
 
-        int k, start, n_unique;
+            singleton = 0;
+            n_unique = unique_values.size();
 
-        for(int i=0; i<n; i++){ // for each data unit
-
-            //data[i]
-            // ideas: our own for loop for k and bool (ci is a singleton)
-            // ideas: function from std count distinct values in vector
-            // ideas: using a (multi)map?
-            start=0;
-            n_unique=unique_values.size();
-            if(card[allocations[i]]==1){ // datum i is a singleton
-            k=n_unique-1;
-            aux_unique_values[0]=unique_values[allocations[i]];
-            singleton=1;}
-            else k=n_unique;
-            card[allocations[i]]-=1;
-
-            for(int j=singleton;j<m; j++ ){
-            aux_unique_values[j][0] = stan::math::normal_rng(hypers.get_m0(), hypers.get_k0(), rng);
-            aux_unique_values[j][1] = stan::math::inv_gamma_rng(hypers.get_alpha0(),hypers.get_beta0(), rng);
-            }
-            probas = Eigen::VectorXd(k+m); //           std::vector<double> probas(k+m,0);
-
-            for(int k=0; k<n_unique ; k++){ // if datum i is a singleton card[k] when k=allocations[i] is equal to 0 ->probas[k]=0
-                probas(k)=card[k]*stan::math::normal_cdf(data[i],unique_values[k][0],unique_values[k][1])/(n-1+ mixture.get_totalmass());
-            }
-            for(int k=0; k<m ; k++){
-                probas(n_unique+k)=(mixture.get_totalmass()/m)*stan::math::normal_pdf(data[i],unique_values[k][0],unique_values[k][1])/(n-1+ mixture.get_totalmass());
-            }
-
-            int c_new=  stan::math::categorical_rng(probas,rng) //std::discrete_distribution(probas); // in stats??
-            if(singleton){
-
-                if (c_new>=n_unique){ // SINGLETON - AUX
-                    unique_values[allocations[i]]=aux_unique_values[c_new-n_unique];
-                    card[allocations[i]]+=1;
-                }
-                else // SINGLETON - OLD VALUE
-            {unique_values.erase(unique_values.begin()+allocations[i]-1);
-                card.erase(card.begin()+allocations[i]-1);
-                card[c_new]+=1;
-
-                int tmp=allocations[i];
-                allocations[i]=c_new;
-                for(auto &c:allocations){ // relabeling
-                    if (c>tmp)
-                    c-=1;
-                }
-            }
-
+            if(card[ allocations[i] ] == 1){ // datum i is a singleton
+                k = n_unique - 1;
+                aux_unique_values[0] = unique_values[ allocations[i] ];
+                singleton = 1;
             }
             else{
+                k = n_unique;
+            }
 
-                if (c_new>=n_unique){ // NOT SINGLETON - AUX
-                    unique_values.push_back(aux_unique_values[c_new-n_unique]);
+            card[ allocations[i] ] -= 1;
+
+            for(int j=singleton;j<m; j++){
+                aux_unique_values[j][0] = stan::math::normal_rng(
+                    hypers.get_m0(), hypers.get_k0(), rng);
+                aux_unique_values[j][1] = stan::math::inv_gamma_rng(
+                    hypers.get_alpha0(),hypers.get_beta0(), rng);
+            }
+
+            auto probas = Eigen::VectorXd(k+m); // or vec initialized to 0
+
+            for(int k=0; k<n_unique ; k++){ // if datum i is a singleton, then
+                // card[k] when k=allocations[i] is equal to 0 ->probas[k]=0
+                probas(k) = card[k] * stan::math::normal_cdf(data[i], // TODO cdf? sure?
+                    unique_values[k][0], unique_values[k][1]) / (
+                    n-1+mixture.get_totalmass() );
+            }
+            for(int k=0; k<m ; k++){
+                probas(n_unique+k) = (mixture.get_totalmass()/m) *
+                    stan::math::normal_pdf(data[i],unique_values[k][0],
+                    unique_values[k][1]) / ( n-1+mixture.get_totalmass() );
+            }
+
+            unsigned int c_new = stan::math::categorical_rng(probas,rng);
+            if(singleton == 1){
+                if (c_new >= n_unique){ // case 1 of 4: SINGLETON - AUX
+                    unique_values[ allocations[i] ] =
+                        aux_unique_values[c_new-n_unique];
+                    card[allocations[i]] += 1;
+                }
+                else{ // case 2 of 4: SINGLETON - OLD VALUE
+                    unique_values.erase(
+                        unique_values.begin()+allocations[i]-1 );
+
+                    card.erase( card.begin()+allocations[i]-1 );
+                    card[c_new] += 1;
+
+                    int tmp = allocations[i];
+                    allocations[i] = c_new;
+                    for(auto &c : allocations){ // relabeling
+                        if (c > tmp)
+                            c -= 1;
+                    }
+                }
+            } // end of if(singleton == 1)
+
+            else{ // if singleton == 0
+
+                if (c_new>=n_unique){ // case 3 of 4: NOT SINGLETON - AUX
+                    unique_values.push_back( aux_unique_values[c_new-n_unique] );
                     card.push_back(1);
-                    allocations[i]=n_unique+1;
-            }
-            else{ // NOT SINGLETON - OLD VALUES
-                allocations[i]=c_new;
-                card[c_new]+=1;
-            }
+                    allocations[i] = n_unique + 1;
+                }
+                else{ // case 4 of 4: NOT SINGLETON - OLD VALUES
+                    allocations[i] = c_new;
+                    card[c_new] += 1;
+                }
 
+            }
+        } // end of for(int i=0; i<n; i++) loop
+
+    } // end of sample_allocations()
+
+
+
+
+    void sample_unique_values(){
+        std::vector<std::vector<unsigned int>> clust_idxs;
+        for(int i=0; i<n; i++) // save different cluster in each row
+            clust_idxs[ allocations[i] ].push_back(i);
+         
+        for (int j=0; j< clust_idxs.size(); j++) {
+            std::vector<data_t> curr_data;
+
+            for ( auto &idx : clust_idxs[j] )
+                curr_data.push_back( data[idx] );
+
+            hierarchies(j).sample(curr_data);
         }
-            }
-        } // end sample allocation
-
-
-
-
-void sample_unique_values(){
-std::vector<std::vector<unsigned int>> clust_idxs;
-for(int i=0; i<n; i++) // save different cluster in each row
-    clust_idxs[allocations[i]].push_back(i);
-
-
-for (int j=0; j< clust_idxs.size(); j++) {
-    std::vector<data_t> curr_data;
-    for (auto &idx: clust_idxs[j]){
-        curr_data.push_back(data[idx]);
     }
-    hierarchies(j).sample(curr_data);
-}
 
-}
+
+} // end of Class Neal8
 
 
 
