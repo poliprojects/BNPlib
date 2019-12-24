ab15bc897887aec03bc55d51d18ce701b8879fb1
Bruno Guindani <bruno@guindani.org>
2019-12-24 18:26:42 +0100

    Cleanup

--- Neal8_NNIG.hpp
+++ Neal8_NNIG.hpp
@@ -9,14 +9,15 @@ template<typename Hypers> //Hypers = TupleWrapper, distro, ...
 class NNIGHierarchy {
 protected:
     using std::tuple<par_t, par_t, par_t, par_t> = state_tuple_t;
+
     unsigned int rng = 20191225;
-    state_tuple_t state; // current values for G0's
-        // parameters: in order, mu_0, sig2_0, alpha, beta
+    state_tuple_t state; // current values for G0's parameters: in order, mu_0,
+                         // sig2_0, alpha, beta
 
     std::shared_ptr<Hypers> hypers;
 
 public:
-    // getters/setters:
+    // Getters/setters:
     state_tuple_t get_state(){return state;}
     void set_state(const state_tuple_t &s){state = s;}
     void set_state(int pos, par_t val){state(pos) = val;}
@@ -26,26 +27,28 @@ public:
     }
 
     void draw() {
-        real sigmaNew = stan::math::inv_gamma_rng(hypers.get_alpha0(),hypers.get_beta0(), rng);
-        real muNew = stan::math::normal_rng(hypers.get_m0(), hypers.get_k0(), rng);
+        real sigmaNew = stan::math::inv_gamma_rng(hypers.get_alpha0(),
+            hypers.get_beta0(), rng);
+        real muNew = stan::math::normal_rng(hypers.get_m0(), hypers.get_sig0(),
+            rng);
         state(0) = muNew;
         state(1) = sigmaNew;
         }
 
     void sample_given_data(std::vector<data_t> data) { //TODO
-        // get current values of parameters
+        // Get current values of parameters
         auto mu_0    = hypers.get_current_val(0);
         auto sig2_0  = hypers.get_current_val(1);
         auto alpha_0 = hypers.get_current_val(2);
         auto beta_0  = hypers.get_current_val(3);
 
-        // compute posterior parameters //TODO
+        // Compute posterior parameters //TODO
         auto mu_post = ...;
         auto sig_post = ...;
         auto alpha_post = ...;
         auto beta_post = ...;
 
-        // get a sample
+        // Get a sample
         par_t sigma_new = stan::math::inv_gamma_rng(alpha_post, beta_post, rng);
         par_t mu_new = stan::math::normal_rng(mu_post, sig_post * sigma_new, rng);
         state(0) = mu_new;
@@ -57,19 +60,20 @@ public:
 
 
 
-template<class Hierarchy, class Hypers>
+template<class Hierarchy, class Hypers, unsigned int n_aux = 3>
 class Neal8{
 private:
-    int m = 3; // TODO
-    int maxiter = 10000;
-    int burnin = 1000;
+    unsigned int m = n_aux; // TODO
+    unsigned int maxiter = 1000;
+    unsigned int burnin = 0;
+
     //std::vector<Hierarchy> hierarchies;
     std::vector<data_t> data;
-    std::array<n,int> allocations; // the c vector
-    std::vector<Hierarchy> unique_values;
-    std::array<m,Hierarchy> aux_unique_values;
+    std::array<n, unsigned int> allocations; // the c vector
+    std::vector<Hierarchy> unique_values; // the phi vector
+    std::array<m, Hierarchy> aux_unique_values; // the auxiliary phi values
     
-    Hypers hypers; // see Hypers class in other files
+    Hypers hypers;
 
 
     void initalize(){
@@ -82,12 +86,11 @@ private:
         sample_unique_values();
     }
 
-
     void sample_allocations(){
         // Other ideas:
-        // * ideas: our own for loop for k and bool (ci is a singleton)
-        // * ideas: function from std count distinct values in vector
-        // * ideas: using a (multi)map?
+        // * our own for loop for k and bool (ci is a singleton)
+        // * function from std count distinct values in vector
+        // * using a (multi)map?
 
         // Initialize some relevant variables
         unsigned int k, n_unique, singleton;
@@ -104,7 +107,8 @@ private:
 
             if(card[ allocations[i] ] == 1){ // datum i is a singleton
                 k = n_unique - 1;
-                aux_unique_values[0].setState(unique_values[allocations[i]].getState()); // move phi value in aux
+                aux_unique_values[0].set_state(unique_values[allocations[i]
+                    ].get_state()); // move phi value in aux
                 singleton = 1;
             }
             else{
@@ -114,33 +118,33 @@ private:
             card[ allocations[i] ] -= 1;
 
 
-             // draw from G0 the aux
-
+            // Draw the aux from G0
             for(int j=singleton;j<m; j++){
                 aux_unique_values[j].draw();
             }
 
-
-            //draw a NEW value for ci
+            // Draw a NEW value for ci
             Eigen::Matrix<double, k+m, 1> probas;
-            // or std::vec initialized to 0, or dynamic Eigen::VectorXd(k+m)
 
             for(int k=0; k<n_unique ; k++){ // if datum i is a singleton, then
-                // card[k] when k=allocations[i] is equal to 0 ->probas[k]=0
+                // card[k] when k=allocations[i] is equal to 0 -> probas[k]=0
 
-                // TODO // meglio in logscale (?)
+                auto M = mixture.get_totalmass();
+
+                // TODO meglio in logscale (?)
                 probas(k) = card[k] * unique_values[k].loglike(data[i]) / (
-                    n-1+mixture.get_totalmass() );
+                    n-1+M);
             }
             for(int k=0; k<m ; k++){
-                probas(n_unique+k) = (mixture.get_totalmass()/m) *
-                    aux_unique_values[k].loglike(data[i])/ ( n-1+mixture.get_totalmass() );
+                probas(n_unique+k) = (M/m) *
+                    aux_unique_values[k].loglike(data[i]) / (n-1+M);
             }
 
             unsigned int c_new = stan::math::categorical_rng(probas,rng);
             if(singleton == 1){
                 if (c_new >= n_unique){ // case 1 of 4: SINGLETON - AUX
-                    unique_values[ allocations[i] ].setState(aux_unique_values[c_new-n_unique].getState());
+                    unique_values[ allocations[i] ].set_state(
+                        aux_unique_values[c_new-n_unique].get_state());
                     card[allocations[i]] += 1;
                 }
                 else{ // case 2 of 4: SINGLETON - OLD VALUE
@@ -162,7 +166,7 @@ private:
             else{ // if singleton == 0
 
                 if (c_new>=n_unique){ // case 3 of 4: NOT SINGLETON - AUX
-                    unique_values.push_back( aux_unique_values[c_new-n_unique] );
+                    unique_values.push_back(aux_unique_values[c_new-n_unique]);
                     card.push_back(1);
                     allocations[i] = n_unique + 1;
                 }
@@ -175,7 +179,6 @@ private:
         } // end of for(int i=0; i<n; i++) loop
 
     } // end of sample_allocations()
-
 
 
 
@@ -195,10 +198,8 @@ private:
     }
 
 
-    void save_iteration(unsigned int iter){
+    void save_iteration(unsigned int iter){ // TODO
         std::cout << "Iteration n. " << iter << " / " << maxiter << std::endl;
-        for(auto &s : state)
-            std::cout << s << " " << std::endl;
     }
 
 
@@ -220,6 +221,3 @@ public:
 int main() {
     Neal8<NNIGHierarchy<HypersFixed>> sampler;
 }
-
-// TODO:
-// * in sample_allocations(), use vec of hierarchies instead of vec of params
