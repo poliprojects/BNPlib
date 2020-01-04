3266a9b1d7343dd40cf28da49c9e5652b02fd0b9
Bruno Guindani <bruno@guindani.org>
2020-01-04 21:41:22 +0100

    lol

---
+++ test_main.cpp
@@ -0,0 +1,11 @@
+#include <stan/math.hpp>
+//#include <armadillo>
+//#include "statslib/stats.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << "log normal(1 | 2, 3)="
+  //          << stan::math::normal_log(1, 2, 3)
+            << std::endl;
+  return 0;
+}
