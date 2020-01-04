2b73abb9116b5e2b7a002cb297cff5763004b603
Bruno Guindani <bruno@guindani.org>
2020-01-04 22:43:02 +0100

    sigh

---
+++ run.sh
@@ -0,0 +1,7 @@
+g++ \
+	-I /usr/include/eigen3 \
+	-I ./stan-math/lib/tbb_2019_U8/include/ \
+	-I ./stan-math/lib/sundials_4.1.0/include/ \
+	-c test_main.cpp -o test.o
+
+#/home/bruno/Desktop/bnplib/stan-math/
