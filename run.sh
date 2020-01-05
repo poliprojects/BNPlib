2bf27a04ba2ef38c5099bd163f925d7fa4bc07e5
Bruno Guindani <bruno@guindani.org>
2020-01-05 19:46:05 +0100

    Deleted run stuff and error.txt

--- run.sh
+++
@@ -1,9 +0,0 @@
-g++ -D_REENTRANT \
-	-I ./stan-math/lib/eigen_3.3.3 \
-	-I ./stan-math/lib/tbb_2019_U8/include/ \
-	-I ./stan-math/lib/sundials_4.1.0/include/ \
-	-o test.o test_main.cpp
-
-#/home/bruno/Desktop/bnplib/stan-math/
-
-
