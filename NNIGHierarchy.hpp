5f545e3a8e8afb37c13fff255edd62609a0c92ef
Bruno Guindani <bruno@guindani.org>
2020-02-01 19:13:17 +0100

    Removed arma stuff

--- NNIGHierarchy.hpp
+++ NNIGHierarchy.hpp
@@ -2,14 +2,12 @@
 template<class Hypers> //Hypers = TupleWrapper, distro, ...
 class NNIGHierarchy {
 protected:
-    using state_t= std::array<par_t,2>;
+    using state_t = std::array<par_t,2>;
 
     std::mt19937 rng;
-
     state_t state; // current values for F's parameters: mu, sigma
-
     std::shared_ptr<Hypers> hypers; // current values for G0's parameters:
-                                    // mu_0,Lambda0, alpha, beta
+                                    // mu_0, lambda0, alpha, beta
 
 public:
     // Constructors:
@@ -23,64 +21,68 @@ public:
 
     int get_count(){return hypers.use_count();}
 
-
     double log_like(data_t datum) {
         return exp(stan::math::normal_lpdf(datum, state[0], state[1]));
     }
 
     void draw() {
-        float sigmaNew = stan::math::inv_gamma_rng(hypers->get_alpha0(),
+        float sigma_new = stan::math::inv_gamma_rng(hypers->get_alpha0(),
             hypers->get_beta0(), rng);
-        float muNew = stan::math::normal_rng(hypers->get_mu0(),
-            sigmaNew/hypers->get_lambda(), rng);
-        state[0] = muNew;
-        state[1] = sigmaNew;
+        float mu_new = stan::math::normal_rng(hypers->get_mu0(),
+            sigma_new/hypers->get_lambda(), rng);
+        state[0] = mu_new;
+        state[1] = sigma_new;
     }
 
 
     void sample_given_data(std::vector<data_t> data) {
         // Get current values of parameters
-        auto mu0    = hypers->get_mu0();
-        auto Lambda0  = hypers->get_lambda();
-        auto alpha0 = hypers->get_alpha0();
-        auto beta0  = hypers->get_beta0();
+        auto mu0     = hypers->get_mu0();
+        auto lambda0 = hypers->get_lambda();
+        auto alpha0  = hypers->get_alpha0();
+        auto beta0   = hypers->get_beta0();
 
-        arma::vec temp = normalGammaUpdate(data, mu0, alpha0, beta0, Lambda0);
+        parvec_t temp = normalGammaUpdate(data, mu0, alpha0, beta0, lambda0);
 
         auto mu_post = temp(0);
         auto alpha_post = temp(1);
         auto beta_post = temp(2);
-        auto postLambda = temp(3);
+        auto lambda_post = temp(3);
 
         // Get a sample
         par_t sigma_new = stan::math::inv_gamma_rng(alpha_post, beta_post, rng);
-        par_t mu_new = stan::math::normal_rng(mu_post, sigma_new/postLambda,
-            rng); //? is it ok /postLambda?
+        par_t mu_new = stan::math::normal_rng(mu_post, sigma_new/lambda_post,
+            rng); //? is it ok /lambda_post?
         state[0] = mu_new;
         state[1] = sigma_new;
     }
 
 
 
-    arma::vec normalGammaUpdate(arma::vec data, double mu0, double alpha0,
-        double beta0,double Lambda0) {
-            double mu_post, alpha_post, beta_post, postLambda;
-            int n = data.size();
-            if (n == 0)
-                return arma::vec{mu0, alpha0, beta0, Lambda0};
-    double ybar = arma::mean(data);
-    mu_post = (Lambda0 * mu0 + n * ybar) / (Lambda0 + n);
-    alpha_post = 1.0 * alpha0 + 1.0 * n / 2;
+    parvec_t normalGammaUpdate(std::vector<data_t> data, double mu0,
+        double alpha0, double beta0, double lambda0) {
+        
+        double mu_post, alpha_post, beta_post, lambda_post;
+        int n = data.size();
+        if (n == 0)
+            return parvec_t{mu0, alpha0, beta0, lambda0};
 
-    // arma::var(x, 1) divides by n, not n-1
-    double ss = n * arma::var(data, 1);
+        // Compute sample mean
+        double y_bar = accumulate(data.begin(), data.end(), 0.0) / n;
 
-    beta_post = (beta0 + 0.5 * ss + 0.5 * Lambda0 /
-        (n + Lambda0) * n * std::pow((ybar - mu0), 2));
+        mu_post = (lambda0 * mu0 + n * y_bar) / (lambda0 + n);
+        alpha_post = 1.0 * alpha0 + 1.0 * n / 2;
 
-    postLambda = Lambda0 + n;
+        // double ss = n * arma::var(data, 1); // divides by n, not n-1
+        double ss = std::inner_product( data.begin(), data.end(), data.begin(),
+            0.0 ) - n*y_bar*y_bar; // TODO check
 
-    return arma::vec{mu_post, alpha_post, beta_post, postLambda};
-  }
+        beta_post = (beta0 + 0.5 * ss + 0.5 * lambda0 /
+            (n + lambda0) * n * std::pow((y_bar - mu0), 2));
+
+        lambda_post = lambda0 + n;
+
+        return parvec_t{mu_post, alpha_post, beta_post, lambda_post};
+    }
 
 };
