800f59ac3dfffce4588d878bbafa5d62c4b8b685
Bruno Guindani <bruno@guindani.org>
2020-02-14 16:07:58 +0100

    R scripts

--- R/densities.R
+++ R/densities.R
@@ -4,22 +4,20 @@ cards = read.csv("../clust_cardinalities.csv", header=F)
 
 # Pre-processing
 data  = as.numeric(data[1:(length(data)-1)])
-cards = cards[1:(length(cards)-1)]
-
-#print(dim(cards))
+cards = t( cards[1:(length(cards)-1)] )
 
 svg(filename="density_pure.svg")
-hist(data, freq=T)
+hist(data, freq=F, ylim=c(0.0, 0.3))
 lines(dens)
+#abline(v=4, col=1)
+#abline(v=6, col=2)
 dev.off()
 
-# TODO check if it works
-#svg(filename="cardinalities_all.svg")
-#plot(cards)
-#dev.off()
+svg(filename="cardinalities_all.svg")
+plot(cards)
+dev.off()
 
-#thin = seq(1,length(cards),500)
-#print(thin)
-#svg(filename="cardinalities_thinned.svg")
-#plot(cards[thin])
-#dev.off()
+thin = seq(1, length(cards), 100)
+svg(filename="cardinalities_thinned.svg")
+plot(thin,cards[thin])
+dev.off()
