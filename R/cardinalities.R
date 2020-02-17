b07626394294aed47bd7db5cbf189aab83a9f333
Bruno Guindani <bruno@guindani.org>
2020-02-17 10:36:22 +0100

    Cleanup

--- R/cardinalities.R
+++ R/cardinalities.R
@@ -1,9 +1,6 @@
-data  = read.csv("../data.csv", header=F)
-dens  = read.csv("../density0.25.csv", header=F)
 cards = read.csv("../clust_cardinalities.csv", header=F)
 
 # Pre-processing
-data  = as.numeric(data[1:(length(data)-1)])
 cards = t( cards[1:(length(cards)-1)] )
 
 #pdf("cardinalities_all.pdf")
@@ -12,5 +9,5 @@ cards = t( cards[1:(length(cards)-1)] )
 
 thin = seq(1, length(cards), 100)
 pdf("cardinalities_thinned.pdf")
-plot(thin,cards[thin])
+plot(thin, cards[thin])
 dev.off()
--- R/rng_test.R
+++ R/rng_test.R
@@ -2,5 +2,5 @@ half = 50
 data = read.csv("data.csv", header=F)
 data = as.numeric(data)
 hist(data)
-hist(data[0:(half-1)])
-hist(data[half:(2*half-1)])
+hist(data[1:half])
+hist(data[(half+1):(2*half)])
