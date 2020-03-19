3a5ab117e3f3e9471e7c90d1f503ec865c5d9b81
Bruno Guindani <bruno@guindani.org>
2020-03-19 14:54:23 +0100

    meh

--- main.cpp
+++ main.cpp
@@ -46,14 +46,13 @@ int main(int argc, char *argv[]){
   
     HypersFixedNNIG hy(5.0, 1.0, 2.0, 2.0); // mu0, lambda, alpha0, beta0
     DirichletMixture mix(1); // total mass
-    Neal2<HierarchyNNIG, HypersFixedNNIG, DirichletMixture> sampler2(
+    Neal8<HierarchyNNIG, HypersFixedNNIG, DirichletMixture> sampler(
         data, mix, hy);
-    //Neal8<HierarchyNNIG, HypersFixedNNIG, DirichletMixture> sampler8(
+    //Neal8<HierarchyNNIG, HypersFixedNNIG, DirichletMixture> sampler(
       //  data, 3, mix, hy);
 	
     // Run sampler(s)
-    sampler2.run();
-    //sampler8.run();
+    sampler.run();
 
     //Neal8<HierarchyNNIG, HypersFixedNNIG, DirichletMixture> sampler(
       // 	data, n_aux, mix, hy);
@@ -72,22 +71,22 @@ int main(int argc, char *argv[]){
         grid<<temp;
         temp += step;
     }
-    //sampler8.eval_density(grid);
-    //sampler8.write_density_to_file("density_m50.csv");
+    //sampler.eval_density(grid);
+    //sampler.write_density_to_file("density_m50.csv");
 
-    sampler2.eval_density(grid);
-    //sampler2.write_density_to_file("densityneal2.csv");
-	//unsigned int i_cap = sampler2.cluster_estimate();
+    sampler.eval_density(grid);
+    //sampler.write_density_to_file("densityneal2.csv");
+	//unsigned int i_cap = sampler.cluster_estimate();
     //std::cout << "Best clustering: at iteration " << i_cap << std::endl;
-    //sampler2.write_final_clustering_to_file();
-    //sampler2.write_best_clustering_to_file();
+    //sampler.write_final_clustering_to_file();
+    //sampler.write_best_clustering_to_file();
 
     // Clustering stuff
-    //unsigned int i_cap = sampler8.cluster_estimate();
+    //unsigned int i_cap = sampler.cluster_estimate();
     //std::cout << "Best clustering: at iteration " << i_cap << std::endl;
-    //sampler8.write_final_clustering_to_file("clust_final0.25.csv");
-    //sampler8.write_best_clustering_to_file("clust_best1.csv");
-    //sampler8.write_chain_to_file();
+    //sampler.write_final_clustering_to_file("clust_final0.25.csv");
+    //sampler.write_best_clustering_to_file("clust_best1.csv");
+    //sampler.write_chain_to_file();
 
     //return 0;
 }
