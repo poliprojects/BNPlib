95b60f85b34ec395006cf6686ed35192489e15ac
zazzetti <elena.zazzetti@mail.polimi.it>
2020-04-19 02:12:40 -0700

    add const mixture

--- src/mixtures/DirichletMixture.hpp
+++ src/mixtures/DirichletMixture.hpp
@@ -9,23 +9,23 @@ private:
 public:
     ~DirichletMixture() = default;
 
-    DirichletMixture(const double totalmass): totalmass(totalmass){
+    DirichletMixture(const double &totalmass): totalmass(totalmass){
         assert(totalmass >= 0);
     }
 
     // Compute probabilities
-    double const prob_existing_cluster(const int card, const unsigned int n){
+    double const prob_existing_cluster(const int &card, const unsigned int &n){
     	return card/(n-1+totalmass);
     }
     
-    double const prob_new_cluster(const unsigned int n,
-    	const unsigned int n_unique){
+    double const prob_new_cluster(const unsigned int &n,
+    	const unsigned int &n_unique){
     	return totalmass/(n-1+totalmass);
     }
 
     // Getters and setters
     double const get_totalmass(){return totalmass;}
-    void set_totalmass(const double totalmass_){totalmass = totalmass_;}
+    void set_totalmass(const double &totalmass_){totalmass = totalmass_;}
 };
 
 #endif // DIRICHLETMIXTURE_HPP
--- src/mixtures/PitYorMixture.hpp
+++ src/mixtures/PitYorMixture.hpp
@@ -10,27 +10,27 @@ private:
 public:
     ~PitYorMixture() = default;
 
-    PitYorMixture(const double strength, const double discount):
+    PitYorMixture(const double &strength, const double &discount):
         strength(strength), discount(discount){
         assert(strength > -discount);
         assert(0 <= discount && discount<1);
     }
 
     // Compute probabilities
-    double const prob_existing_cluster(const int card, const unsigned int n){
+    double const prob_existing_cluster(const int &card, const unsigned int &n){
         return (card-discount)/(n-1+strength);
     }
 
-    double const prob_new_cluster(const unsigned int n,
-        const unsigned int n_unique){
+    double const prob_new_cluster(const unsigned int &n,
+        const unsigned int &n_unique){
         return (strength+discount*n_unique)/(n-1+strength);
     }
     
     // Getters and setters
     double const get_strength(){return strength;}
     double const get_discount(){return discount;}
-    void set_strength(const double strength_){strength = strength_;}
-    void set_discount(const double discount_){discount = discount_;}
+    void set_strength(const double &strength_){strength = strength_;}
+    void set_discount(const double &discount_){discount = discount_;}
 };
 
 #endif // DIRICHLETMIXTURE_HPP
