a5e65d6fc45d61d5d89a05be09ada57c6960ce36
Bruno Guindani <bruno@guindani.org>
2020-04-19 13:30:34 +0200

    Factory WIP

--- src/algorithms/Factory.hpp
+++ src/algorithms/Factory.hpp
@@ -1,21 +1,21 @@
 #ifndef FACTORY_HPP
 #define FACTORY_HPP
-
 #include <map>
 #include <vector>
 #include <memory>
 #include <functional>
-
-#include "Neal2.hpp"
-#include "Neal8.hpp"
+//#include <stdexcept>, #include <type_traits>, #include <sstream>
 
 
-template<template <class> class Hierarchy, class Hypers, class Mixture>
+
+
+
+template<class AbstractProduct>
 class Factory{
 private:
     // Aliases
-    using AlgoBuilder = std::function<
-        std::unique_ptr<Algorithm<Hierarchy, Hypers, Mixture>>() >;
+    using Builder = std::function< std::unique_ptr<AbstractProduct>() >;
+
     using Identifier = std::string;
 
     // Deleted constructors
@@ -24,50 +24,61 @@ private:
     Factory& operator=(const Factory &f) = delete;
 
     // Storage for algorithm builders
-    std::map<Identifier, AlgoBuilder> storage;
+    std::map<Identifier, Builder> storage;
 
-public:
-    // Destructor
-    ~Factory() = default;
-    
-    static Factory& Instance(){
-        static Factory factory;
-        return factory;
+  static Factory& Instance(){
+  // We use the Meyer's trick to istantiate the factory as Singleton
+  static Factory theFactory;
+  return theFactory;
+}
+
+  template<typename... Args>
+  std::unique_ptr<AbstractProduct> create(Identifier const & name, Args&&... args) 
+  const {
+  auto f = storage.find(name);
+  if (f == storage.end())
+    {
+      std::stringstream idAsString;
+      //I am assuming that identifier has a output streaming operator
+      idAsString<<name;
+td::string out="Identifier " + idAsString.str() +
+ " is not stored in the factory";
+hrow std::invalid_argument(out);
     }
-
-    template <typename ...Args>
-    auto create_algorithm_object(const Identifier &name, Args&&... args) const {
-        switch(name){
-            case "neal2":
-                return std::make_unique<Neal2<Hierarchy, Hypers, Mixture>>(
-                std::forward<Args>(args)...);
-            case "neal8":
-                return std::make_unique<Neal8<Hierarchy, Hypers, Mixture>>(
-                std::forward<Args>(args)...);
-            default:
-                return std::unique_ptr<Algorithm<Hierarchy, Hypers, Mixture>>(
-                std::forward<Args>(args)...);
-        }
+  else
+    {
+      return std::make_unique<AbstractProduct>(f->second(
+        std::forward<Args>(args)...));
+      //return f->second(std::forward<Args>(args)...);
     }
-
-    void add_builder(const Identifier &name, const AlgoBuilder &builder){
-        auto f = storage.insert(std::make_pair(name, builder));
-        if(f.second == false){
-            std::cout <<
-                "Warning: new duplicate builder was not added to storage" <<
-                std::endl;
-        }
+}
+  //! Register the given rule.
+  void add(Identifier const & name, 
+                                                 Builder const & func){
+  auto f = 
+    storage.insert(std::make_pair(name, func));
+  if (f.second == false)
+    {
+      std::stringstream idAsString;
+      idAsString <<name;
+      std::string message=std::string(
+        "Double registration in Factory of id: ")+idAsString.str()+
+        std::string(" is not allowed");
+      throw std::invalid_argument(message);
     }
-
-    std::vector<Identifier> list_of_known_builders() const {
-        std::vector<Identifier> tmp;
-        tmp.reserve(storage.size());
-        for(auto i = storage.begin(); i != storage.end(); i++){
-            tmp.push_back(i->first);
-        }
-        return tmp;
-    }
-
+}
+  //! Returns a list of registered rules.
+  std::vector<Identifier> registered()const {
+  std::vector<Identifier> tmp;
+  tmp.reserve(storage.size());
+  for(auto i=storage.begin(); i!=storage.end();++i)
+    tmp.push_back(i->first);
+  return tmp;
+}
+  //! Unregister a rule.
+  void unregister(Identifier const & name){ storage.erase(name);}
+  //! Destructor
+  ~Factory()=default;
 };
 
 
