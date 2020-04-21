82dac0ccee5e5780472382cb0cbfc057121431a5
Bruno Guindani <bruno@guindani.org>
2020-04-22 00:52:37 +0200

    sigh

--- src/api/BaseCollector.hpp
+++ src/api/BaseCollector.hpp
@@ -34,10 +34,10 @@ public:
 
     // Getters and setters
     State get_next_state(){
+        curr_iter++;
     	if(curr_iter >= size){
     		throw std::out_of_range("Error: curr_iter >= size in collector");
     	}
-    	curr_iter++;
     	return next_state();
     }
 
