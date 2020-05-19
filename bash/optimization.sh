3e4fe8a87de68b2c50d618a49e3dc74948643e5b
Bruno Guindani <bruno@guindani.org>
2020-05-19 10:37:04 +0200

    opti.sh and csvs

--- bash/optimization.sh
+++ bash/optimization.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-valgrind --tool=callgrind ./maintest_uni csv/data_uni.csv neal2 memory
+#valgrind --tool=callgrind ./maintest_uni csv/data_uni.csv neal2 memory
+valgrind --tool=callgrind ./maintest_multi csv/data_multi_2cl.csv neal2 memory
