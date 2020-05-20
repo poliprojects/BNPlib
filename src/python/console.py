6c2a35d127911df17ed73fa9d09ce27aa2ac22a6
Bruno Guindani <bruno@guindani.org>
2020-05-20 21:57:44 +0200

    console.py cleanup

--- src/python/console.py
+++ src/python/console.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python
-print("Importing...")
 from bnp_interface import *
 
-print("Done")
 mu0 = 5.0
 lambda_ = 0.1
 alpha0 = 2.0
 beta0 = 2.0
 totalmass = 1.0
 datafile = "csv/data_uni.csv"
-#grid = [0.5*_ for _ in range(20)]
-gridfile = "csv/grid_uni.csv"
-
-
-
 algo = "neal8"
+rng = 20200229
+maxit = 5000
+burn = 500
+
 coll_type = "file"
 filecoll_name = "collector.recordio"
+
+#grid = [0.5*_ for _ in range(20)]
+gridfile = "csv/grid_uni.csv"
 densfile = "src/python/density.csv"
-imgfile = "src/python/plot.pdf"
-rng = 20200229
-maxit = 1000
-burn = 100
+imgfile  = "src/python/plot.pdf"
 
 bnplib.run_NNIG_Dir(mu0, lambda_, alpha0, beta0, totalmass, datafile, algo,
 	coll_type, filecoll_name, rng, maxit, burn)
 
 algo = "neal8_dataless"
-#bnplib.estimates_NNIG_Dir(mu0, lambda_, alpha0, beta0, totalmass, gridfile,
-#	algo, filecoll_name, densfile)
- # TODO implement estimates s.t. you do not have to write "dataless"
+bnplib.estimates_NNIG_Dir(mu0, lambda_, alpha0, beta0, totalmass, gridfile,
+	algo, filecoll_name, densfile)
+# TODO implement estimates cpp s.t. you don't have to write "_dataless"
 
-plot_density(densfile)
-
-#plot_density("csv/dens_multi.csv")
+plot_density(densfile, imgfile)
