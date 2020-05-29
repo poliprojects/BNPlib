67dca517d3f91eda79041a770583d4eb3226669e
Bruno Guindani <bruno@guindani.org>
2020-05-29 14:54:56 +0200

    Transferred multi_utiles to bnp_interface + tabs -> spaces

--- src/python/bnp_interface.py
+++ src/python/bnp_interface.py
@@ -2,79 +2,96 @@ from google.protobuf.internal.encoder import _VarintBytes
 from google.protobuf.internal.decoder import _DecodeVarint32
 import output_pb2
 
-import os, sys
+import csv, os, sys
 import matplotlib.pyplot as plt
 import numpy as np
-import csv
 from mpl_toolkits.mplot3d import Axes3D
-
-
 from scipy.special import comb
+
 LIBPATH = ''.join((os.path.dirname(os.path.realpath(__file__)), "/../.."))
 sys.path.insert(0, LIBPATH)
 import bnplibpy
 
 
 def deserialize(collfile = "collector.recordio"):
-	with open(collfile, 'rb') as f:
-		buf = f.read()
-		n = 0
-		d = []
-		while n < len(buf):
-			msg_len, new_pos = _DecodeVarint32(buf, n)
-			n = new_pos
-			msg_buf = buf[n:n+msg_len]
-			n += msg_len
-			read_metric = output_pb2.State()
-			read_metric.ParseFromString(msg_buf)
-			d.append(read_metric)
-	return[d]
+    with open(collfile, 'rb') as f:
+        buf = f.read()
+        n = 0
+        d = []
+        while n < len(buf):
+            msg_len, new_pos = _DecodeVarint32(buf, n)
+            n = new_pos
+            msg_buf = buf[n:n+msg_len]
+            n += msg_len
+            read_metric = output_pb2.State()
+            read_metric.ParseFromString(msg_buf)
+            d.append(read_metric)
+    return[d]
+
+
+def get_grid(d):
+    uni_g = np.arange(-5, +5.1, 0.5)
+    arr = [uni_g for y in range(d)]
+    mesh= np.meshgrid(*arr)
+    nrows=len(mesh[0].flat)
+    grid = np.zeros((nrows,d))
+
+
+    for i in range(0,d):
+        grid[:,i] = mesh[i].flat
+        
+    return[grid]
+
+
+def empirical_mean(datafile):
+    mat = np.loadtxt(open(datafile, 'rb'), delimiter=' ')
+    return[np.mean(mat, axis=0)]
 
 
 def chain_histogram(collfile = "collector.recordio",
-	imgfile = "src/python/chain.pdf"):
-	d = deserialize(collfile)
-	figure = plt.figure()
-	num_clusters = []
-	
-	for i in d[0]:  
-		num_clusters.append(len(i.uniquevalues))
+    imgfile = "src/python/chain.pdf"):
+    d = deserialize(collfile)
+    figure = plt.figure()
+    num_clusters = []
+    
+    for i in d[0]:  
+        num_clusters.append(len(i.uniquevalues))
 
-	plt.hist(num_clusters)
-	plt.savefig(imgfile)
-	print("Successfully saved plot to", imgfile)
+    plt.hist(num_clusters)
+    plt.savefig(imgfile)
+    print("Successfully saved plot to", imgfile)
 
 
 def plot_density(densfile = "src/python/density.csv",
-	imgfile = "src/python/density.pdf"):
-	"""TODO docstring.
+    imgfile = "src/python/density.pdf"):
+    """TODO docstring.
 
-	TODO docstring but longer."""
-	mat = np.loadtxt(open(densfile, 'rb'), delimiter=',')
+    TODO docstring but longer."""
+    mat = np.loadtxt(open(densfile, 'rb'), delimiter=',')
 
-	cols = mat.shape[1]
-	if cols not in (2,3):
-		print("Error: density file must have 2-3 columns to be plotted")
-		return
-	figure = plt.figure()
-	if cols == 2:
-		ax = figure.add_subplot(111)
-		plt.plot(mat[:,0], mat[:,1])
-	else: # if cols == 3
-		ax = figure.add_subplot(111, projection='3d')
-		ax.scatter(mat[:,0], mat[:,1], mat[:,2])
-	plt.savefig(imgfile)
-	print("Successfully saved plot to", imgfile)
+    cols = mat.shape[1]
+    if cols not in (2,3):
+        print("Error: density file must have 2-3 columns to be plotted")
+        return
+    figure = plt.figure()
+    if cols == 2:
+        ax = figure.add_subplot(111)
+        plt.plot(mat[:,0], mat[:,1])
+    else: # if cols == 3
+        ax = figure.add_subplot(111, projection='3d')
+        ax.scatter(mat[:,0], mat[:,1], mat[:,2])
+    plt.savefig(imgfile)
+    print("Successfully saved plot to", imgfile)
 
 
 def plot_clust_cards(clustfile = "src/python/clust.csv",
-	imgfile = "src/python/clust.pdf"):
-	mat = np.loadtxt(open(clustfile, 'rb'), delimiter=',')
-	figure = plt.figure()
-	plt.hist(mat[:,0])
-	plt.savefig(imgfile)
-	print("Successfully saved plot to", imgfile)
-	
+    imgfile = "src/python/clust.pdf"):
+    mat = np.loadtxt(open(clustfile, 'rb'), delimiter=',')
+    figure = plt.figure()
+    plt.hist(mat[:,0])
+    plt.savefig(imgfile)
+    print("Successfully saved plot to", imgfile)
+    
 
 def rand_index_score(clusters, classes):
     tp_plus_fp = comb(np.bincount(clusters), 2).sum()
@@ -86,14 +103,15 @@ def rand_index_score(clusters, classes):
     fn = tp_plus_fn - tp
     tn = comb(len(A), 2) - tp - fp - fn
     return (tp + tn) / (tp + fp + fn + tn)
-    
-def print_clust_rand_indx(clustfile = "src/python/clust.csv",
-	trueclustfile = "src/csv/test/true_clust.csv"):
-	mat_pred = np.loadtxt(open(clustfile, 'rb'), delimiter=',')
-	mat_true = np.loadtxt(open(trueclustfile, 'rb'), delimiter=',')
-	values=np.unique(mat_pred[:,1])
-	new_indx=np.arange(len(values))
-	for i in range(0,mat_pred.shape[0]):
-		mat_pred[i,0]=new_indx[np.where(values==mat_pred[i,1])]
-	print("Rand index score:",rand_index_score(mat_pred[:,0].astype(int), mat_true.astype(int)))
 
+
+def print_clust_rand_indx(clustfile = "src/python/clust.csv",
+    trueclustfile = "src/csv/test/true_clust.csv"):
+    mat_pred = np.loadtxt(open(clustfile, 'rb'), delimiter=',')
+    mat_true = np.loadtxt(open(trueclustfile, 'rb'), delimiter=',')
+    values=np.unique(mat_pred[:,1])
+    new_indx=np.arange(len(values))
+    for i in range(0,mat_pred.shape[0]):
+        mat_pred[i,0]=new_indx[np.where(values==mat_pred[i,1])]
+    print("Rand index score:",rand_index_score(mat_pred[:,0].astype(int),
+        mat_true.astype(int)))
--- src/python/test5-8_multi.py
+++ src/python/test5-8_multi.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from bnp_interface import *
-from multi_utils import *
 
 lambda_ = 0.2;
 totalmass = 1
@@ -16,25 +15,22 @@ only = "all"
 d=[2,5,10,20]
 c=[5] # TODO per plot
 for i in c:
-	print("Starting test", i)
-	datafile  = ''.join(("csv/test/data", str(i), ".csv"))
-	collfile  = ''.join(("collector", str(i), ".recordio"))
-	densfile  = ''.join(("src/python/test_res/dens",  str(i), ".csv"))
-	clustfile = ''.join(("src/python/test_res/clust", str(i), ".csv"))
-	imgfileclust = ''.join(("src/python/test_res/clust", str(i), ".pdf"))
-	imgfilechain = ''.join(("src/python/test_res/chain", str(i), ".pdf"))
-	imgfiledens  = ''.join(("src/python/test_res/dens",  str(i), ".pdf"))
-	mu0 = empirical_mean(datafile)
-	nu=d[i-5]+3
-	tau0 = (1/nu) * np.identity(d[i-5])
-	bnplibpy.run_NNW_Dir(mu0, lambda_,tau0, nu, totalmass, datafile, algo,colltype, collfile, rng, maxit, burn)
-	chain_histogram(collfile, imgfilechain)
-	grid=get_grid(d[i-5])
-	bnplibpy.estimates_NNW_Dir(mu0, lambda_, tau0, nu, totalmass, grid[0], algoDL,
-	collfile, densfile, clustfile, only)
-	plot_clust_cards(clustfile, imgfileclust)
-	plot_density(densfile, imgfiledens)
-
-
-
-
+    print("Starting test", i)
+    datafile  = ''.join(("csv/test/data", str(i), ".csv"))
+    collfile  = ''.join(("collector", str(i), ".recordio"))
+    densfile  = ''.join(("src/python/test_res/dens",  str(i), ".csv"))
+    clustfile = ''.join(("src/python/test_res/clust", str(i), ".csv"))
+    imgfileclust = ''.join(("src/python/test_res/clust", str(i), ".pdf"))
+    imgfilechain = ''.join(("src/python/test_res/chain", str(i), ".pdf"))
+    imgfiledens  = ''.join(("src/python/test_res/dens",  str(i), ".pdf"))
+    mu0 = empirical_mean(datafile)
+    nu=d[i-5]+3
+    tau0 = (1/nu) * np.identity(d[i-5])
+    bnplibpy.run_NNW_Dir(mu0, lambda_,tau0, nu, totalmass, datafile, algo,
+        colltype, collfile, rng, maxit, burn)
+    chain_histogram(collfile, imgfilechain)
+    grid=get_grid(d[i-5])
+    bnplibpy.estimates_NNW_Dir(mu0, lambda_, tau0, nu, totalmass, grid[0],
+        algoDL, collfile, densfile, clustfile, only)
+    plot_clust_cards(clustfile, imgfileclust)
+    plot_density(densfile, imgfiledens)
